d9c37c34d613f3997146928dbb7e7922cda4179a
fferio <fabio.ferio@gmail.com>
2015-03-30 04:25:57 -0300

    correcao da craft e exit

--- Assets/Scenes/Craft/Craft.cs
+++ Assets/Scenes/Craft/Craft.cs
@@ -10,6 +10,9 @@ public class Craft : MonoBehaviour {
 	Texture2D _craftSubTitle;
 
 	[SerializeField]
+	Texture2D _scrollBg;
+
+	[SerializeField]
 	Texture2D _create;
 
 	[SerializeField]
@@ -21,17 +24,26 @@ public class Craft : MonoBehaviour {
 	[SerializeField]
 	Texture2D _ok;
 
+	//private List<Magia> magias;
+
 
 	void OnGUI()
 	{
-
+		//texture de BG
 		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), _craftMenuBg);
 
+		//texture do subtitulo
 		GUI.DrawTexture(new Rect(Screen.width - _craftSubTitle.width -20, 20, _craftSubTitle.width, _craftSubTitle.height), _craftSubTitle);
 
+		//magias = new List<magia>();
+		//magias.Add (new magia ());
+
+
+		//botoes create, edit e delete
 		if (GUI.Button(new Rect(Screen.width - 50 - _create.width - 10, 150, _create.width, _create.height), _create))
 		{
-
+			//Debug.Log("Clicked the button with an image");
+			Application.LoadLevel("magicCraft");
 		}
 
 		if (GUI.Button(new Rect(Screen.width - 50 - _edit.width - 10, 150 + _edit.height + 20, _edit.width, _edit.height), _edit))
@@ -44,8 +56,10 @@ public class Craft : MonoBehaviour {
 			
 		}
 
+		//botao de saida
 		if (GUI.Button(new Rect(Screen.width - 30 - _ok.width - 10, Screen.height - 30 - _ok.height - 10, _ok.width, _ok.height), _ok))
 		{
+			//Object.DontDestroyOnLoad
 			Application.LoadLevel("Main");
 		}
 	}
--- Assets/Scenes/Exit/Exit.cs
+++ Assets/Scenes/Exit/Exit.cs
@@ -18,22 +18,28 @@ public class Exit: MonoBehaviour {
 	[SerializeField]
 	Texture2D _naoButton;
 
+	private float telaX;
+	private float telaY;
+
 	void OnGUI()
 	{
+		telaX = Screen.width/2;
+		telaY = Screen.height/2;
+
 		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), _mainBg);
 
-		GUI.Box (new Rect (200, 200, Screen.width - 400, Screen.height - 400), "");
+		//GUI.Box (new Rect (telaX/2, telaY/2, Screen.width/2, Screen.height/2), "");
 
-		GUI.DrawTexture(new Rect (200, 200, Screen.width - 400, Screen.height - 400), _exitBg);
+		GUI.DrawTexture(new Rect (telaX/2, telaY/2, Screen.width/2, Screen.height/2), _exitBg);
 
-		GUI.DrawTexture(new Rect (275, 200, Screen.width - 550, Screen.height - 650), _certeza);
+		GUI.DrawTexture(new Rect (telaX - (Screen.width - 550)/2, telaY/2, Screen.width - 550, Screen.height - 650), _certeza);
 
-		if (GUI.Button (new Rect (370, 500, 120, _simButton.height), _simButton))
+		if (GUI.Button (new Rect (telaX - (3*_simButton.width)/2, telaY + _simButton.height, _simButton.width - 5, _simButton.height), _simButton))
 		{
 			Application.Quit();
 		}
 
-		if (GUI.Button (new Rect (575, 500, 120, _naoButton.height), _naoButton))
+		if (GUI.Button (new Rect (telaX + _naoButton.width/2, telaY + _naoButton.height, _naoButton.width - 5, _naoButton.height), _naoButton))
 		{
 			Application.LoadLevel("Main");	
 		}
